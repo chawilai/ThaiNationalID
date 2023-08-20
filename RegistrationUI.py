aae137863b29e0002cd01e79932aae8ea77031da
chawilai <wat.chawilai@gmail.com>
2023-08-21 00:14:24 +0700

    fix make it working

--- RegistrationUI.py
+++ RegistrationUI.py
@@ -1,71 +1,77 @@
-from tkinter import *
-import tkinter.messagebox
+import tkinter as tk
+from tkinter import messagebox
 
-class RegistrationUI(parent=Frame):
+class RegistrationUI(tk.Frame):
     def __init__(self, parent):
-        Frame.__init__(self, parent)
+        super().__init__(parent)
         self.parent = parent
-        self.initUI()
-        self.religion_list = ["Christianity" , "Buddihsm" , "Islam" , "Sikh", "Hindu"]
-
-    def initUI(self):
         self.parent.title("สุ่มเลขบัตรประชาชน - ลงทะเบียนเข้าสู่ระบบ")
-        self.pack(fill=BOTH, expand=1)
+        self.parent.geometry('600x700+200+100')
+
+        # Define religion_list as an instance variable
+        self.religion_list = ["Christianity", "Buddhism", "Islam", "Sikh", "Hindu"]
+
+        # Create a BooleanVar to track the check state
+        self.religion_status_var = tk.BooleanVar()
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.pack(fill=tk.BOTH, expand=1)
 
         # New contact grid
-        Label(self, text="Generated Citizen ID: ").grid(row=0, column=2, columnspan=2, sticky=W)
-        Label(self, text="First Name:").grid(row=1, column=1, sticky=E)
-        Label(self, text="Last Name:").grid(row=2, column=1, sticky=E)
-        Label(self, text="Address").grid(row=3, column=1, sticky=E)
+        tk.Label(self, text="Generated Citizen ID: ").grid(row=0, column=2, columnspan=2, sticky=tk.W)
+        tk.Label(self, text="First Name:").grid(row=1, column=1, sticky=tk.E)
+        tk.Label(self, text="Last Name:").grid(row=2, column=1, sticky=tk.E)
+        tk.Label(self, text="Address").grid(row=3, column=1, sticky=tk.E)
 
-        self.entry1 = Entry(self)
-        self.entry2 = Entry(self)
-        self.entry3 = Entry(self)
+        self.entry1 = tk.Entry(self)
+        self.entry2 = tk.Entry(self)
+        self.entry3 = tk.Entry(self)
         self.entry1.grid(row=1, column=2)
         self.entry2.grid(row=2, column=2)
         self.entry3.grid(row=3, column=2)
 
-        religion_status_check = IntVar()
-        self.religion_status_check = Checkbutton(self, variable=religion_status_check,
+        religion_status_check = tk.IntVar()
+        religion_status_check = tk.Checkbutton(self, variable=self.religion_status_var,
                                         command=self.require_religion,
                                         text="Specify Religion?")
-        self.religion_status_check.grid(row=4, column=2, columnspan=2)
+        religion_status_check.grid(row=4, column=2, columnspan=2)
 
-        Label(self, text="Birthday:").grid(row=5, column=1, sticky=E)
-        Label(self, text="Religion:").grid(row=6, column=1, sticky=E)
+        tk.Label(self, text="Birthday:").grid(row=5, column=1, sticky=tk.E)
+        tk.Label(self, text="Religion:").grid(row=6, column=1, sticky=tk.E)
 
-        self.entry4 = Entry(self)
-        self.entry5 = Entry(self)
+        self.entry4 = tk.Entry(self)
+        self.entry5 = tk.Entry(self)
 
         self.entry4.grid(row=5, column=2)
         self.entry5.grid(row=6, column=2)
 
-        religion_list = Listbox(self, width=20, height=len(self.religion_list))
+        religion_list = tk.Listbox(self, width=20, height=len(self.religion_list))
         religion_list.grid(row=0, column=1)
         for religion in self.religion_list:
-            religion_list.insert(END, religion)
+            religion_list.insert(tk.END, religion)
 
-        religion_list.bind("<<ListboxSelect>>", self.onSelect)
+        religion_list.bind("<<ListboxSelect>>", self.on_select)
         religion_list.place(x=20, y=210)
 
-
-    def onSelect(self, val):
-        sender = val.widget
-        idk = sender.curselection()
-        value = sender.get(idk)
-        self.entry5.set(value)
+    def on_select(self, event):
+        sender = event.widget
+        selected_index = sender.curselection()
+        if selected_index:
+            selected_value = sender.get(selected_index[0])
+            self.entry5.delete(0, tk.END)
+            self.entry5.insert(0, selected_value)
 
     def require_religion(self):
-        if self.religion_status_check.get() == 1:
+        if self.religion_status_var.get():  # Check the state using get()
             self.entry5.config(state='disabled')
         else:
-            self.entry5.config(state='enabled')
-
+            self.entry5.config(state='normal')
 
 def main():
-    root = Tk()
+    root = tk.Tk()
     ex = RegistrationUI(root)
-    root.geometry('600x700+200+100')
     root.mainloop()
 
 if __name__ == '__main__':
--- ThaiNationalIDGenerator.py
+++ ThaiNationalIDGenerator.py
@@ -1,22 +1,16 @@
-from tkinter import *
+import tkinter as tk
 import tkinter.messagebox
 import webbrowser
 import random
 
-'''โปรแกรมสุ่มเลขบัตรประชาชน
-Thai national ID number generator
-'''
-
-
-class ThaiNationalID_Generator:
-    # Generate ID number randomly
+class ThaiNationalIDGenerator:
     def generate_number(self):
         random_num = random.randint(100000000000, 199999999999)
         temp = random_num
         sumall = 0
 
-        for i in range(1,13):
-            sumall += (i+1) * (random_num % 10)
+        for i in range(1, 13):
+            sumall += (i + 1) * (random_num % 10)
             random_num = random_num // 10
 
         sumall = sumall % 11
@@ -24,9 +18,8 @@ class ThaiNationalID_Generator:
         result = (temp * 10) + checksum
         self.thai_id.set(result)
 
-    # Enable Copy-to-clipboard function
     def copy_button(self):
-        clip = Tk()
+        clip = tk.Tk()
         clip.withdraw()
         clip.clipboard_clear()
         clip.clipboard_append(self.thai_id.get())
@@ -38,41 +31,39 @@ class ThaiNationalID_Generator:
     def about_box(self):
         tkinter.messagebox.showinfo("About", "Thai National ID Generator\
         \nโปรแกรมสุ่มเลขบัตรประชาชน\n\nBuilt with love by Coregame\nhttps://coregame-th.com")
-        
-    def __init__(self, outputEntry=None):
-        window = Tk()
-        window.title("สุ่มเลขบัตรประชาชน")
 
-        # Menu Bar
-        menubar = Menu(window)
-        window.config(menu = menubar)
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("สุ่มเลขบัตรประชาชน")
 
-        commandmenu = Menu(menubar, tearoff = 0)
-        menubar.add_cascade(label = "Command", menu = commandmenu)
-        commandmenu.add_command(label = "Generate", command = self.generate_number)
-        commandmenu.add_command(label = "Copy to Clipboard", command = self.copy_button)
+        menubar = tk.Menu(self.window)
+        self.window.config(menu=menubar)
+
+        commandmenu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Command", menu=commandmenu)
+        commandmenu.add_command(label="Generate", command=self.generate_number)
+        commandmenu.add_command(label="Copy to Clipboard", command=self.copy_button)
         commandmenu.add_separator()
-        commandmenu.add_command(label = "Exit", command = window.destroy)
+        commandmenu.add_command(label="Exit", command=self.window.destroy)
 
-        aboutmenu = Menu(menubar, tearoff = 0)
-        menubar.add_cascade(label = "About", menu = aboutmenu)
-        aboutmenu.add_command(label = "Github", command = self.open_github)
+        aboutmenu = tk.Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="About", menu=aboutmenu)
+        aboutmenu.add_command(label="Github", command=self.open_github)
         aboutmenu.add_separator()
-        aboutmenu.add_command(label = "About", command = self.about_box)
+        aboutmenu.add_command(label="About", command=self.about_box)
 
-        # Main Program
-        Label(window, text = "Thai National ID Generator").grid(row = 1, column = 1, sticky = W)
-        Label(window, text = "Your number is ").grid(row = 2, column = 1, sticky = W)
-        
-        self.thai_id = StringVar()
-        Entry(window, textvariable = self.thai_id,justify = RIGHT, state = 'readonly').grid(row = 2, column = 2, sticky = E)
-        outputEntry.configure()
-        
-        Button(window, text = "Generate",
-               command = self.generate_number).grid(row = 4, column = 2, sticky = E)
-        Button(window, text = "Copy to Clipboard",
-               command = self.copy_button).grid(row = 4, column = 1, sticky = W)
-        
-        window.mainloop()  
+        tk.Label(self.window, text="Thai National ID Generator").grid(row=1, column=1, sticky=tk.W)
+        tk.Label(self.window, text="Your number is ").grid(row=2, column=1, sticky=tk.W)
 
-ThaiNationalID_Generator()
+        self.thai_id = tk.StringVar()
+        tk.Entry(self.window, textvariable=self.thai_id, justify=tk.RIGHT, state='readonly').grid(row=2, column=2, sticky=tk.E)
+
+        tk.Button(self.window, text="Generate", command=self.generate_number).grid(row=4, column=2, sticky=tk.E)
+        tk.Button(self.window, text="Copy to Clipboard", command=self.copy_button).grid(row=4, column=1, sticky=tk.W)
+
+    def run(self):
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    app = ThaiNationalIDGenerator()
+    app.run()
